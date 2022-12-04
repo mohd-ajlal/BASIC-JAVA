ed3826fb77f6b03c7c0f7eb08395322d366f056e
Mohd Ajlal <ajlal00786@gmail.com>
2022-12-04 23:02:26 +0530

    JAVA

--- Conditionals/src/Main.java
+++ Conditionals/src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args)
+    {
+        int x = 15;
+        int y = 5;
+        System.out.println("Before swapping:" + " x = " + x + ", y = " + y);
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        System.out.println("After swapping:" + " x = " + x + ", y = " + y);
     }
 }
-
