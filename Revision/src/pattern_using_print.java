5668242e3d3879693daee282b6daaa7b13f14a7b
Mohd Ajlal <ajlal00786@gmail.com>
2023-02-03 01:18:00 +0530

    JAVA

--- Revision/src/pattern_using_print.java
+++ Revision/src/pattern_using_print.java
@@ -1,2 +1,9 @@
-public class pattern_using_print {
+public class pattern_using_print{
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
 }
