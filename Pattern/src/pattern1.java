6cf026f4b4f2a0cf5de7c4581a75480111e1189a
Mohd Ajlal <ajlal00786@gmail.com>
2023-02-05 00:24:16 +0530

    JAVA

--- Pattern/src/pattern1.java
+++ Pattern/src/pattern1.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
-public class Main {
+public class pattern1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter no. of rows: ");
         int row = sc.nextInt();
-        for(int i = 1;  ;i++){
-
+        for(int i = 1; i<=row ;i++){
+            for(int j= 0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
--- Pattern/src/pattern2.java
+++ Pattern/src/pattern2.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
-public class Main {
+public class pattern2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter no. of rows: ");
         int row = sc.nextInt();
-        for(int i = 1;  ;i++){
-
+        for(int i = 1; i<=row ;i++){
+            for(int j = 1; j<=row-i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
