ba40a81b2cba30bb22c82cc8b3a6cd678552a502
Mohd Ajlal <ajlal00786@gmail.com>
2023-06-23 03:58:47 +0530

    JAVA

---
+++ Pattern/src/JAVA_loop_2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class JAVA_loop_2{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for(int j = 0; j<n; j++){
+                int sum = a;
+                for(int k = 0; k<=j; k++){
+                    sum += Math.pow(2,k)*b;
+                }
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+            }
+        }
+        // in.close();
+    }
+
--- Pattern/src/binary_to_decimal.java
+++ Pattern/src/binary_to_decimal.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+// import java.lang.Math;
 public class binary_to_decimal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
