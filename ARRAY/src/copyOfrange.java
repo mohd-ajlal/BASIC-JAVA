ffc77cb3eaaa3b95cbe32bf7265282a11d09ed60
Mohd Ajlal <ajlal00786@gmail.com>
2023-06-28 04:10:18 +0530

    JAVA

---
+++ ARRAY/src/copyOfrange.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class copyOfrange {
+        static void array_input(int arr[], int size){
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i<size; i++){
+            System.out.print("Enter the value of "+i+" index: ");
+            arr[i] = sc.nextInt();
+        }
+    }
+    static void print_array(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        array_input(arr, size);
+        int arr_new[] = Arrays.copyOfRange(arr, 1, 3);
+        System.out.println("The new array is: ");
+        print_array(arr_new);
+    }
+}
