fa21f31c313eba95c0119c82797c93c805a423cc
Mohd Ajlal <ajlal00786@gmail.com>
2023-06-28 04:09:56 +0530

    JAVA

---
+++ ARRAY/src/clone.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class clone {
+    static void array_input(int arr[], int size){
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i<size; i++){
+            System.out.print("Enter the value of "+i+" index: ");
+            arr[i] = sc.nextInt();
+        }
+    }
+    static void print_array(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        array_input(arr, size);
+        int arr_new[] = arr.clone();
+        System.out.println("The new array is: ");
+        print_array(arr_new);
+    }    
+}
