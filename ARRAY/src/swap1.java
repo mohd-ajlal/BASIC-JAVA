940d2bb1bd904c3196a93c25bedaf2701af5c905
Mohd Ajlal <ajlal00786@gmail.com>
2023-07-10 02:30:54 +0530

    JAVA

---
+++ ARRAY/src/swap1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class swap1 {
+    static void print_original(int a, int b){
+        System.out.println("Original value of a: "+a);
+        System.out.println("Original value of b: "+b);
+    }
+
+    static int[] swap(int a, int b){
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        int arr[] = {a, b};
+        return arr;
+    }
+
+    static void print_swapped(int a, int b){
+        System.out.println("Swapped value of a: "+a);
+        System.out.println("Swapped value of b: "+b);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter value of a: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter value of b: ");
+        int b = scanner.nextInt();
+        System.out.println();
+        print_original(a, b);
+        int arr[] = swap(a, b);
+        System.out.println();
+        print_swapped(arr[0], arr[1]);
+    }
+}
