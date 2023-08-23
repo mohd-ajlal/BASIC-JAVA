b32c133cf3633b6e18c9b167994e86f75125929f
Mohd Ajlal <ajlal00786@gmail.com>
2023-08-24 03:22:13 +0530

    JAVA

---
+++ Basic-Class/src/Insomnia_cure.java
@@ -0,0 +1,21 @@
+// Insomnia cure - codeforces
+
+import java.util.Scanner;
+
+public class Insomnia_cure {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int l = sc.nextInt();
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int d = sc.nextInt();
+        int count = 0;
+        for (int i = 1; i <= d; i++) {
+            if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
